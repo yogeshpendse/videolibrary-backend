e7381e0f48bb6d583df51190e8e8430ea4ec3e0d
yogeshpendse <61093894+yogeshpendse@users.noreply.github.com>
2021-10-16 05:31:35 +0530

    add model changes

--- models/video.model.js
+++ models/video.model.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
-const Videoschema = new mongoose.Schema({
-  videoid: { type: String },
-  name: { type: String },
-});
+const Videoschema = new mongoose.Schema(
+  {
+    videocode: { type: String },
+    name: { type: String },
+    time: { type: Number },
+    name: { type: String },
+    description: { type: String },
+    stars: { type: Number },
+    type: { type: String },
+    prime: { type: Boolean },
+    date: { type: String },
+    datenos: { type: Number },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Video = mongoose.model("Videodatalist", Videoschema);
 
